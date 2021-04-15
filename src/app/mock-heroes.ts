11882d8f595b00922bf358f63309281c8ed2a8ca
garsan84 <jgrsan84@gmail.com>
2021-04-15 19:23:26 +0200

    completed part without server

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Blain' },
+  { id: 12, name: 'Sergius AI' },
+  { id: 13, name: 'Charlie-electronBot' },
+  { id: 14, name: 'XUSmagneton' },
+  { id: 15, name: 'Pilarico reactive' },
+  { id: 16, name: 'Dr Quevedo Mejode' },
+  { id: 17, name: '100_pixelitos' },
+  { id: 18, name: 'Fake-Paco-luna' },
+  { id: 19, name: 'Coronel BOT' },
+  { id: 20, name: 'Trump reactive'}
+];
